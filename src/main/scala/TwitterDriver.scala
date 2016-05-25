850b64cdf34910c959bb3a0f302c0471e3e574f3
aseastman <aeastman@arrow.com>
2016-05-25 17:39:16 -0600

    I had to force the authorization. Let's hope this works.

--- src/main/scala/TwitterDriver.scala
+++ src/main/scala/TwitterDriver.scala
@@ -2,6 +2,10 @@ import org.apache.spark.streaming.dstream.DStream
 import org.apache.spark.streaming.twitter._
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
+import twitter4j.auth.{AuthorizationFactory, OAuthAuthorization}
+import twitter4j.conf.ConfigurationBuilder
+
+import scala.io.Source
 //import twitter4j.Status
 
 
@@ -14,12 +18,27 @@ object TwitterDriver {
     val sc = new SparkContext(conf)
     val ssc = new StreamingContext(sc,Seconds(10))
 
+    val config : Iterator[String] = Source.fromFile("twitter4j.properties").getLines()
+    val consumerKey       = config.find(_.startsWith("oauth.consumerKey")).get.split("=")(1)
+    val consumerSecret    = config.find(_.startsWith("oauth.consumerSecret")).get.split("=")(1)
+    val accessToken       = config.find(_.startsWith("oauth.accessToken")).get.split("=")(1)
+    val accessTokenSecret = config.find(_.startsWith("oauth.accessTokenSecret")).get.split("=")(1)
+
+    val cb = new ConfigurationBuilder()
+    cb.setDebugEnabled(true)
+      .setOAuthConsumerKey(consumerKey)
+      .setOAuthConsumerSecret(consumerSecret)
+      .setOAuthAccessToken(accessToken)
+      .setOAuthAccessTokenSecret(accessTokenSecret)
+
+    val auth = AuthorizationFactory.getInstance(cb.build())
+
     val filters = Seq("#Android")
 
-    val tweets = TwitterUtils.createStream(ssc, None)
+    val tweets = TwitterUtils.createStream(ssc, Option(auth),filters)
 
 
-    tweets.foreachRDD(tweetRDD => println(tweetRDD.take(1)))
+    tweets.foreachRDD(tweetRDD => tweetRDD.foreach(println))
 
 
 
