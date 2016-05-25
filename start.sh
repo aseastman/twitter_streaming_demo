d682053ce8a7697b546540f7a9837db2e74a4162
aseastman <aeastman@arrow.com>
2016-05-25 16:01:04 -0600

    There was an issue with one of the jar files

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 env HADOOP_CONF_DIR=/etc/hive/conf spark-submit --master yarn-cluster \
 --conf spark.akka.frameSize=200 --files /etc/hive/conf/hive-site.xml \
---jars /home/a78084/.ivy2/cache/org.apache.spark/spark-streaming-twitter_2.10/jars/spark-streaming-twitter_2.10-1.5.0.jar, \
-/home/a78084/.ivy2/cache/org.twitter4j/twitter4j-core/jars/twitter4j-core-3.0.3.jar, \
-/home/a78084/.ivy2/cache/org.twitter4j/twitter4j-stream/jars/twitter4j-stream-3.0.3.jar \
+--jars /home/a78084/.ivy2/cache/org.apache.spark/spark-streaming-twitter_2.10/jars/spark-streaming-twitter_2.10-1.5.0.jar \
 --num-executors 1 --executor-cores 20 --driver-memory 32G --executor-memory 64G target/scala-2.10/spark-h_2.10-0.1.jar
