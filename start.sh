d3d138a48258a4370ef71f4e1dd13d5c2d637604
aseastman <aeastman@arrow.com>
2016-05-28 17:23:44 -0600

    Still having the following issue: java.lang.NoClassDefFoundError: Could not initialize class org.apache.http.impl.conn.ManagedHttpClientConnectionFactory

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ env HADOOP_CONF_DIR=/etc/hive/conf spark-submit --master yarn-cluster \
 --jars /home/a78084/.ivy2/cache/org.apache.spark/spark-streaming-twitter_2.10/jars/spark-streaming-twitter_2.10-1.5.0.jar,\
 /home/a78084/.ivy2/cache/edu.stanford.nlp/stanford-corenlp/jars/stanford-corenlp-3.6.0.jar,\
 /home/a78084/.ivy2/cache/edu.stanford.nlp/stanford-corenlp/jars/stanford-corenlp-3.6.0-models.jar,\
-/home/a78084/.ivy2/cache/org.apache.httpcomponents/httpclient/jars/httpclient-4.3.2.jar,\
+/home/a78084/.ivy2/cache/org.apache.httpcomponents/httpclient/jars/httpclient-4.3.5.jar,\
+/home/a78084/.ivy2/cache/org.apache.httpcomponents/httpcore/jars/httpcore-4.3.2.jar,\
 /home/a78084/.ivy2/cache/com.googlecode.efficient-java-matrix-library/ejml/jars/ejml-0.23.jar \
 --num-executors 1 --executor-cores 20 --driver-memory 32G --executor-memory 64G target/scala-2.10/spark-h_2.10-0.1.jar
